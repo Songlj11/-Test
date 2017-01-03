e690550b2d1244760505f6699d7b950e3e6e1048
Songlj11 <rich@richdeMacBook-Air.local>
2017-01-03 16:31:45 +0800

    song
    now

--- txtTest/ViewController.m
+++ txtTest/ViewController.m
@@ -7,18 +7,42 @@
 //
 
 #import "ViewController.h"
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
+#define kScreenWidth  [UIScreen mainScreen].bounds.size.width
 
-@interface ViewController ()
-
+@interface ViewController ()<UIWebViewDelegate>
+{
+    UIWebView * _webView;
+}
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //NSLog(@"viewDidLoad-keso");
+    float s=12352.4545646756756757;
+    float a = s/10000;
+    UIView *bv = [[UIView alloc]initWithFrame:CGRectMake(100, 100, 100, 100)];
+    bv.backgroundColor = [UIColor redColor];
+    [self.view addSubview:bv];
+    
+    NSString *strDistance=[NSString stringWithFormat:@"%.2lf", a];
+    NSLog(@"-----%@",strDistance);
 }
-
+-(void)viewWillAppear:(BOOL)animated{
+    NSLog(@"viewWillAppear");
+}
+-(void)viewDidAppear:(BOOL)animated{
+    NSLog(@"viewDidAppear");
+}
+-(void)viewWillDisappear:(BOOL)animated{
+    NSLog(@"viewWillDisappear");
+}
+-(void)viewDidDisappear:(BOOL)animated{
+    NSLog(@"viewDidDisappear");
+    //-http://www.cnblogs.com/xiaofeixiang/
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
